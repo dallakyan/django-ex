759a9a42daa98e3ed99e50526b78f5be99b1aea9
sargis <dallakyans@gmail.com>
2016-07-23 15:33:38 -0700

    d

--- HealthDB/patient/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'HealthDB.patient.views.hello',),
-    
-)
--- HealthDB/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-
-def hello(request):
-	return render(request, 'index.html')
