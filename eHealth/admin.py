9ffca2256c660a3f64e8b2d3c05dd756442fecb5
sargis <dallakyans@gmail.com>
2016-07-24 15:50:43 -0700

    d

---
+++ eHealth/admin.py
@@ -0,0 +1,7 @@
+﻿from django.contrib import admin
+from eHealth.models import *
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Page)
+#admin.site.register(Saved_Page)
---
+++ eHealth/myfunctions.py
@@ -0,0 +1,15 @@
+﻿from textstat.textstat import textstat
+from textblob import TextBlob
+
+
+#Using this method to import all the Text Analysis that
+#is carried out in each of the API search engines
+def text_analysis(x):
+    for result in x:
+        blob = TextBlob(result['summary'])
+        for text in blob.sentences:
+            result['pola'] = '%.2f' % (abs(text.sentiment.polarity*10)/2) #-1 to 1
+            result['subj'] = '%.2f' % (abs(text.sentiment.subjectivity*10)/2) #-1 to 1
+            result['reada'] = '%.2f' % textstat.flesch_kincaid_grade(result['summary'])   #out of 10
+    return x
+
